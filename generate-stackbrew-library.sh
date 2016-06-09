e73a3bd34657b3fb6c87c78e16036efc474ec468
Tianon Gravi <admwiggin@gmail.com>
2016-06-09 10:24:27 -0700

    Convert "generate-stackbrew-library.sh" to output the new 2822-based format

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -1,54 +1,102 @@
 #!/bin/bash
-set -e
+set -eu
 
 latestPythonMajor='3'
 
+self="$(basename "$BASH_SOURCE")"
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-versions=( */ )
-versions=( "${versions[@]%/}" )
-url='git://github.com/docker-library/django'
+pyVersions=( */ )
+pyVersions=( "${pyVersions[@]%/}" )
 
-echo '# maintainer: InfoSiftr <github@infosiftr.com> (@infosiftr)'
+# sort version numbers with highest first
+IFS=$'\n'; pyVersions=( $(echo "${pyVersions[*]}" | sort -rV) ); unset IFS
 
-for version in "${versions[@]}"; do
-	pyMajor="${version%.*}"
-	commit="$(cd "$version" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-	fullVersion="$(grep -m1 'ENV DJANGO_VERSION ' "$version/Dockerfile" | cut -d' ' -f3)"
-	versionAliases=( $fullVersion-$version )
+# get the most recent commit which modified any of "$@"
+fileCommit() {
+	git log -1 --format='format:%H' HEAD -- "$@"
+}
+
+# get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
+dirCommit() {
+	local dir="$1"; shift
+	(
+		cd "$dir"
+		fileCommit \
+			Dockerfile \
+			$(git show HEAD:./Dockerfile | awk '
+				toupper($1) == "COPY" {
+					for (i = 2; i < NF; i++) {
+						print $i
+					}
+				}
+			')
+	)
+}
+
+cat <<-EOH
+# this file is generated via https://github.com/docker-library/django/blob/$(fileCommit "$self")/$self
+
+Maintainers: Tianon Gravi <admwiggin@gmail.com> (@tianon),
+             Joseph Ferguson <yosifkit@gmail.com> (@yosifkit)
+GitRepo: https://github.com/docker-library/django.git
+EOH
+
+# prints "$2$1$3$1...$N"
+join() {
+	local sep="$1"; shift
+	local out; printf -v out "${sep//%/%%}%s" "$@"
+	echo "${out#$sep}"
+}
+
+for pyVersion in "${pyVersions[@]}"; do
+	pyMajor="${pyVersion%.*}"
+
+	commit="$(dirCommit "$pyVersion")"
+
+	fullVersion="$(git show "$commit":"$pyVersion/Dockerfile" | awk '$1 == "ENV" && $2 == "DJANGO_VERSION" { print $3; exit }')"
+
 	versionAliases=()
 	while [ "${fullVersion%[.-]*}" != "$fullVersion" ]; do
-		versionAliases+=( $fullVersion-python$pyMajor )
-		if [ "$pyMajor" = "$latestPythonMajor" ]; then
-			versionAliases+=( $fullVersion )
-		fi
+		versionAliases+=( $fullVersion )
 		fullVersion="${fullVersion%[.-]*}"
 	done
-	versionAliases+=( $fullVersion-python$pyMajor )
+	versionAliases+=(
+		$fullVersion
+		latest
+	)
+
+	variant="python$pyMajor"
+
+	variantAliases=( "${versionAliases[@]/%/-$variant}" )
+	variantAliases=( "${variantAliases[@]//latest-/}" )
+
 	if [ "$pyMajor" = "$latestPythonMajor" ]; then
-		versionAliases+=( $fullVersion )
+		variantAliases+=( "${versionAliases[@]}" )
 	fi
-	versionAliases+=( python$pyMajor )
-	if [ "$pyMajor" = "$latestPythonMajor" ]; then
-		versionAliases+=( latest )
-	fi
-	
+
 	echo
-	for va in "${versionAliases[@]}"; do
-		echo "$va: ${url}@${commit} $version"
-	done
-	
-	for variant in onbuild; do
-		commit="$(cd "$version/$variant" && git log -1 --format='format:%H' -- Dockerfile $(awk 'toupper($1) == "COPY" { for (i = 2; i < NF; i++) { print $i } }' Dockerfile))"
-		
-		versionAliases=( python$pyMajor-$variant )
+	cat <<-EOE
+		Tags: $(join ', ' "${variantAliases[@]}")
+		GitCommit: $commit
+		Directory: $pyVersion
+	EOE
+
+	for subVariant in onbuild; do
+		[ -f "$pyVersion/$subVariant/Dockerfile" ] || continue
+
+		commit="$(dirCommit "$pyVersion/$subVariant")"
+
+		variantAliases=( "$variant-$subVariant" )
 		if [ "$pyMajor" = "$latestPythonMajor" ]; then
-			versionAliases+=( $variant )
+			variantAliases+=( "$subVariant" )
 		fi
-		
+
 		echo
-		for va in "${versionAliases[@]}"; do
-			echo "$va: ${url}@${commit} $version/$variant"
-		done
+		cat <<-EOE
+			Tags: $(join ', ' "${variantAliases[@]}")
+			GitCommit: $commit
+			Directory: $pyVersion/$subVariant
+		EOE
 	done
 done
